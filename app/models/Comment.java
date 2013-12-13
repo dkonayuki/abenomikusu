153a288984e9f63b8fa117b332754e50d8602c57
Manabu Nikaidou <nikaidou.m.aa@m.titech.ac.jp>
2013-12-13 11:04:07 +0900

    make db with no check

---
+++ app/models/Comment.java
@@ -0,0 +1,22 @@
+package models;
+
+import play.*;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+public class Comment extends Model{
+	//public static int id; //	auto make, auto increment
+	public User user;
+	public String content;
+	public Date date;
+	public Photo photo;
+	
+	public Comment(User user, String content,Photo photo){
+		this.user=user;
+		this.content=content;
+		this.date=new Date();
+		this.photo=photo;
+	}
+}
---
+++ app/models/User.java
@@ -0,0 +1,28 @@
+package models;
+
+import play.*;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+public class User extends Model{
+    //public static int id; //	auto make, auto increment
+    public String pass;//digested
+    public String username;
+    public String profile;
+    public String avatar;//URL
+    public String cover;//URL
+    public List<User> folower;
+    public List<Photo> photos;
+    
+    public User(String username, String pass) {
+    	this.username=username;
+    	this.pass=pass;
+    	this.profile="よろしくお願いします。";
+    	this.avatar="";//default icon URL
+    	this.cover="";//default cover URL
+    	this.folower=new ArrayList<User>();
+    	this.photos=new ArrayList<Photo>();
+    }
+}
