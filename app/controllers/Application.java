7951e00a840011c51c92a0497146463f85e5a3d7
Nam Nguyen Hoai <namnh.n.aa@m.titech.ac.jp>
2013-12-20 11:27:54 +0900

    edit header page, add and modify css files

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,20 +15,23 @@ public class Application extends Controller {
         render();
     }
     
-    public static void login_signup(){render();}
-    
-    public static void post_login_info(){
-    }
-    
-    public static void post_signup_info(){}
-        
-    public static void profile(){
+    public static void login_signup() {
     	render();
     }
     
-    public static void editProfile(){
+    public static void post_login_info() {
+    }
+    
+    public static void post_signup_info() {}
+        
+    public static void profile() {
+    	render();
+    }
+    
+    public static void editProfile() {
     	System.out.println("送信されました");
     }
+    
     public static void toppage() {
     	render();
     }
