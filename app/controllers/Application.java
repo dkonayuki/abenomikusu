b88f2126a6ca8698c3cf3f50e0db59d09d00338f
Manabu Nikaidou <nikaidou.m.aa@m.titech.ac.jp>
2014-01-07 11:36:19 +0900

    modelの修正

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,26 +2,53 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import sun.misc.BASE64Encoder;
 
 import models.*;
 
 import javax.persistence.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 
 public class Application extends Controller {
 
     public static void index() {
+    	session.clear();
         render();
     }
     
     public static void login_signup(){render();}
     
-    public static void post_login_info(){
+    public static void post_login_info(){}
+    
+    public static void post_signup_info() throws NoSuchAlgorithmException{
+    	// 標準出力に入力されたパラメータの名前を出力
+        System.out.println(params.all().keySet());
+        String name =params.get("name");
+        String pass =params.get("pass");
+        
+        //TODO　名前かぶってないか確認
+        
+        
+    	// アンケートデータをデータベースに書き込む
+        User user = new User(name, pass);
+        user.save();
+        
+        // 埋め込むべき変数をテンプレートに渡す．
+        // テンプレートからはキー名で参照できる．
+        // テンプレートからは，${entry.id} として該当データで自動生成された
+        // ID番号を取得している． 
+        renderArgs.put("user", user);
+        
+     // Map に結果を蓄え，JSON として出力
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("result", "OK");
+        renderJSON(map);
     }
     
-    public static void post_signup_info(){}
-        
     public static void profile(){
     	render();
     }
--- app/models/Photo.java
+++ app/models/Photo.java
@@ -4,20 +4,25 @@ import play.*;
 import play.db.jpa.*;
 
 import javax.persistence.*;
+
 import java.util.*;
 
+@Entity
 public class Photo extends Model{
     //public static int id; //	auto make, auto increment
-    public String url;
-    public String title;
-    public String caption;
-    public List<String> tag;
-    public User user;
-    public List<Comment> comments;
-    public Date date;
-    public HashMap<Long,Integer> rank;//<userid,point>
+	private String url;
+	private String title;
+	private String caption;
+//	private List<String> tag;
+    private User user;
+//  private List<Comment> comments;
+    private Date date;
+    private HashMap<Long,Integer> rank;//<userid,point>
     
-    public double calc_rank(){
+    public void put_rank(long user_id,int point){
+    	this.rank.put(user_id, point);
+    }
+    public double get_rank(){
     	double sum=0;
     	 Iterator it = rank.keySet().iterator();
          while (it.hasNext()) {
@@ -27,13 +32,29 @@ public class Photo extends Model{
     	return sum/rank.size();
     }
     
+    public void set_url(String url){this.url=url;}
+    public String get_url(){return this.url;}
+    
+    public void set_title(String title){this.title=title;}
+    public String get_title(){return this.title;}
+    
+    public void set_caption(String caption){this.caption=caption;}
+    public String get_caption(){return this.caption;}
+    
+    public void set_user(User user){this.user=user;}
+    public User get_user(){return this.user;}
+    
+    public void set_date(Date date){this.date=date;}
+    public Date get_date(){return this.date;}
+    
+    
     public Photo(String url ,String title ,String caption ,User user){
     	this.url=url;
     	this.title=title;
     	this.caption=caption;
-    	this.tag=new ArrayList<String>();
+//    	this.tag=new ArrayList<String>();
     	this.user=user;
-    	this.comments=new ArrayList<Comment>();
+//    	this.comments=new ArrayList<Comment>();
     	this.date=new Date();
     	this.rank=new HashMap<Long,Integer>();
     }
