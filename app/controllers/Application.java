ca63d02a67c9b5fa83174d9f6ff8a8543fcd6fd3
Manabu Nikaidou <nikaidou.m.aa@m.titech.ac.jp>
2014-01-10 10:40:05 +0900

    login,signup機能作成

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -32,30 +32,56 @@ public class Application extends Controller {
 
     public static void login_signup(){render();}
     
-    public static void post_login_info(){}
+    public static void post_login_info() throws NoSuchAlgorithmException{
+    	// 標準出力に入力されたパラメータの名前を出力
+        System.out.println(params.all().keySet());
+        String name =params.get("name");
+        String pass =params.get("pass");
+        System.out.println(name+" "+pass);
+        Map<String, Object> map = new HashMap<String, Object>();
+        //name,passあってるかの確認
+        List<User> users =User.find("username = ?",	name).fetch();
+        if(users.size()!=1){//名前が存在しないor同名が存在（ありえないが念のため）
+        	map.put("result", "Error: no such name");
+        }else{
+        	if(users.get(0).compare_pass(pass)){
+            	map.put("result", "OK");
+                session.put("login_user", users.get(0));
+        	}else{
+        		map.put("result", "Error: wrong password");
+        	}
+        }
+        // Map に結果を蓄え，JSON として出力
+        renderJSON(map);
+    }
     
     public static void post_signup_info() throws NoSuchAlgorithmException{
     	// 標準出力に入力されたパラメータの名前を出力
         System.out.println(params.all().keySet());
         String name =params.get("name");
         String pass =params.get("pass");
-        
-        //TODO　名前かぶってないか確認
-        
-        
-    	// アンケートデータをデータベースに書き込む
-        User user = new User(name, pass);
-        user.save();
-        
-        // 埋め込むべき変数をテンプレートに渡す．
-        // テンプレートからはキー名で参照できる．
-        // テンプレートからは，${entry.id} として該当データで自動生成された
-        // ID番号を取得している． 
-        renderArgs.put("user", user);
-        
-     // Map に結果を蓄え，JSON として出力
+        System.out.println(name+" "+pass);
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("result", "OK");
+        
+        //名前かぶってないか確認
+        List<User> users =User.find("username = ?",	name).fetch();	
+        if(!users.isEmpty()){//名前被り処理
+        	map.put("result", "Error: the name is already exist");
+        }else{
+        	map.put("result", "OK");
+        	// アンケートデータをデータベースに書き込む
+            User user = new User(name, pass);
+            user.save();
+            
+            // 埋め込むべき変数をテンプレートに渡す．
+            // テンプレートからはキー名で参照できる．
+            // テンプレートからは，${entry.id} として該当データで自動生成された
+            // ID番号を取得している． 
+            renderArgs.put("user", user);
+            
+            session.put("login_user", user);
+        }
+        // Map に結果を蓄え，JSON として出力
         renderJSON(map);
     }
 
@@ -92,6 +118,7 @@ public class Application extends Controller {
     }
     
     public static void toppage() {
+    	System.out.println("1234567890-");
     	render();
     }
 }
--- public/javascripts/exp4main.js
+++ public/javascripts/exp4main.js
@@ -14,32 +14,29 @@ function ChangeTab_login_signup(tabname) {
 	}
 }
 
-function signup_check(){
-	var name=document.signup.inputName_signup.value;
-	var pass1=document.signup.inputPassword_signup1.value;
-	var pass2=document.signup.inputPassword_signup2.value;
-	alert("name: "+name+"\npass1: "+pass1+"\npass2: "+pass2);
+function login_check(){//TODO toppageに行くときにUser情報を何かしらの方法で保存しておく
+	var name=document.login.inputName_login.value;
+	var pass=document.login.inputPassword_login.value;
+	alert("name: "+name+"\npass: "+pass);
 	if(name==""){
 		alert("名前を入力してください");
-	}else if((pass1=="")||(pass2=="")){
+	}else if(pass==""){
 		alert("パスワードを入力してください");
-	}else if(pass1!=pass2){
-		alert("確認用パスワードが一致しません");
-	}else if(confirm("名前は"+name+"でよろしいですか？")){
+	}else{
 		var req = new XMLHttpRequest();
 	    // 送信先のURLを指定
-	    req.open("POST", "/post_signup_info");
+	    req.open("POST", "/post_login_info");
 	    // 結果が帰ってきた際に実行されるハンドラを指定
 	    req.onreadystatechange = function () {
 	        // readyState == 4: 修了
 	        if (req.readyState != 4) {
-	            return;
+	        	return;
 	        }
 	        // status == 200: 成功
 	        if (req.status != 200) {
 	            // 成功しなかった．失敗であることを表示して抜ける．
 	            alert("失敗．");
-	            return;
+	        	return;
 	        }
 	        
 	        // body にはサーバから返却された文字列が格納される．
@@ -49,17 +46,71 @@ function signup_check(){
 	
 	        // 戻ってきた JSON 文字列を JavaScript オブジェクトに変換
 	        var data = eval("(" + body + ")");
-	        // 入力に利用したインプットボックスの値を，サーバから戻ってきた値で書き換える
-//	        document.getElementById(todo_id+"_button").value = data.button_next_value;
-//	        document.getElementById(todo_id+"_state").innerHTML = data.todo_state;
+	        if(data.result=="OK"){
+	        	document.location = "/toppage";
+	        }else{
+	        	alert("名前かパスワードが間違っています");
+	            document.getElementById("inputName_login").value = "";
+	            document.getElementById("inputPassword_login").value = "";
+	        }
 	    }
 	    // Content-Type の指定
 	    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	    // <input id="f"> に入力された文字列をエンコードして送信        
-	    req.send("name=" + enc(name));
-	    req.send("pass=" + enc(pass1));
+	    // <input id="f"> に入力された文字列をエンコードして送信
+	    req.send(["name="+enc(name),"pass="+enc(pass)].join("&"));
 	}
+}
+
+function signup_check(){//TODO toppageに行くときにUser情報を何かしらの方法で保存しておく
+	var name=document.signup.inputName_signup.value;
+	var pass1=document.signup.inputPassword_signup1.value;
+	var pass2=document.signup.inputPassword_signup2.value;
+	//alert("name: "+name+"\npass1: "+pass1+"\npass2: "+pass2);
+	if(name==""){
+		alert("名前を入力してください");
+	}else if((pass1=="")||(pass2=="")){
+		alert("パスワードを入力してください");
+	}else if(pass1!=pass2){
+		alert("確認用パスワードが一致しません");
+        document.getElementById("inputPassword_signup1").value = "";
+        document.getElementById("inputPassword_signup2").value = "";
+	}else if(confirm("名前は"+name+"でよろしいですか？")){
+		var req = new XMLHttpRequest();
+	    // 送信先のURLを指定
+	    req.open("POST", "/post_signup_info");
+	    // 結果が帰ってきた際に実行されるハンドラを指定
+	    req.onreadystatechange = function () {
+	        // readyState == 4: 修了
+	        if (req.readyState != 4) {
+	        	return;
+	        }
+	        // status == 200: 成功
+	        if (req.status != 200) {
+	            // 成功しなかった．失敗であることを表示して抜ける．
+	            alert("失敗．");
+	        	return;
+	        }
+	        
+	        // body にはサーバから返却された文字列が格納される．
+	        var body = req.responseText;
+	        // デバッグ表示 
+	        //alert('body: ' + body);
 	
+	        // 戻ってきた JSON 文字列を JavaScript オブジェクトに変換
+	        var data = eval("(" + body + ")");
+	        if(data.result=="OK"){
+	        	alert("登録が完了しました。");
+	        	document.location = "/toppage";
+	        }else{
+	        	alert("その名前は既に使われています");
+	            document.getElementById("inputName_signup").value = "";
+	        }
+	    }
+	    // Content-Type の指定
+	    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	    // <input id="f"> に入力された文字列をエンコードして送信
+	    req.send(["name="+enc(name),"pass="+enc(pass1)].join("&"));
+	}
 }
 
 //入力文字列を urlencode して返す
